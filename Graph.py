3dae6021170c77183dbd3d6589cfb069d623ca64
Phantom <61557664+1337-Phantom@users.noreply.github.com>
2022-09-13 19:16:23 +0200

    added moore Algorythm

--- Graph.py
+++ Graph.py
@@ -7,7 +7,7 @@ class Graph:
     return self.knoten
   
   def existiertKnoten(self, nameKnoten:str) -> bool:
-    return knoten in self.knoten
+    return nameKnoten in self.knoten
   
   def addKnoten(self, nameKnoten:str):
     self.knoten.append(nameKnoten)
@@ -31,18 +31,71 @@ class Graph:
         nachbarn.append(kante[1])
     return nachbarn
 
-graph = Graph()
-graph.addKnoten("A")
-graph.addKnoten("B")
-graph.addKnoten("C")
-graph.addKnoten("D")
+def mooreAlgorythm(nameStartKnoten, graph: Graph):
+  if graph.existiertKnoten(nameStartKnoten):
+    ergebnis = {nameStartKnoten:0}
+    toCheck = [nameStartKnoten]
+    while len(toCheck) != 0:
+      knotenName = toCheck.pop(0)
+      print(knotenName, graph.getAlleNachbarn(knotenName))
+      for nachbar in graph.getAlleNachbarn(knotenName):
+        if not nachbar in ergebnis.keys():
+          toCheck.append(nachbar)
+          ergebnis[nachbar] = ergebnis[knotenName] + 1
+    return ergebnis
 
-graph.addKante("A", "B")
-graph.addKante("B", "B")
-graph.addKante("B", "C")
-graph.addKante("B", "D")
-graph.addKante("C", "A")
-graph.addKante("C", "B")
+  
+  
 
-print(graph.getAlleNachbarn("B"))
+# graph = Graph()
+# graph.addKnoten("A")
+# graph.addKnoten("B")
+# graph.addKnoten("C")
+# graph.addKnoten("D")
 
+# graph.addKante("A", "B")
+# graph.addKante("B", "B")
+# graph.addKante("B", "C")
+# graph.addKante("B", "D")
+# graph.addKante("C", "A")
+# graph.addKante("C", "B")
+
+# print("Nachbarn", graph.getAlleNachbarn("B"))
+# print("Moore", mooreAlgorythm("A", graph))
+
+graph1 = Graph()
+graph1.addKnoten("A")
+graph1.addKnoten("B")
+graph1.addKnoten("C")
+graph1.addKnoten("D")
+graph1.addKnoten("E")
+graph1.addKnoten("F")
+graph1.addKnoten("G")
+graph1.addKnoten("H")
+
+graph1.addKante("A", "C")
+graph1.addKante("A", "E")
+graph1.addKante("A", "G")
+
+graph1.addKante("B", "C")
+graph1.addKante("B", "D")
+
+graph1.addKante("C", "D")
+graph1.addKante("C", "E")
+graph1.addKante("C", "A")
+
+graph1.addKante("D", "B")
+graph1.addKante("D", "E")
+
+graph1.addKante("E", "D")
+graph1.addKante("E", "G")
+
+graph1.addKante("F", "D")
+graph1.addKante("F", "H")
+
+graph1.addKante("G", "D")
+
+graph1.addKante("H", "G")
+graph1.addKante("H", "F")
+
+print("Moore", mooreAlgorythm("A", graph1))
